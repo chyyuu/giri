9a0bdc9677aa186d2e61a94bc0f575b7ab5f6ae7
chyyuu <yuchen@tsinghua.edu.cn>
2014-06-30 18:49:35 +0800

    update some files, now giri can be compiled in llvm-3.4.2, fedora20

---
+++ config.sh
@@ -0,0 +1,1 @@
+../configure --with-llvmsrc=/ubuntu/chyyuu/develop/llvm-git/mytest/giri/build/../../../src/llvm --with-llvmobj=/ubuntu/chyyuu/develop/llvm-git/mytest/giri/build/../../../build --enable-optimized
