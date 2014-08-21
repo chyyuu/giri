6532c227620f4c8d171a52d4f93a83784f6f9b80
chyyuu <yuchen@tsinghua.edu.cn>
2014-08-21 22:14:45 +0800

    add test just one file:test/UnitTests/test1

---
+++ mytest.sh
@@ -0,0 +1,3 @@
+cd test
+make -f Makefile.chy test
+cd ..
---
+++ setenv.sh
@@ -0,0 +1,4 @@
+#should set to llvm-3.4.2
+LROOT=/ubuntu/chyyuu/develop/llvm-git
+export PATH=${LROOT}/install/bin:${LROOT}/src/llvm/tools/clang/toos/scan-view:${LROOT}/src/llvm/tools/clang/tools/scan-build:$PATH
+
